0d58eef8ade1b9071006b3d729f2d7ebd6608b12
Shovik Poddar <shovikmetal@gmail.com>
2020-05-31 21:15:14 +0530

    Whether a, b and c can be the sides of a triangle

---
+++ TriangleTester.java
@@ -0,0 +1,8 @@
+package Competitive;
+class TriangleTester{
+  public static boolean isTriangle(int a, int b, int c){
+    if(a+b>c && b+c>a && a+c>b)
+      return true;
+    return false;  
+  }
+}
